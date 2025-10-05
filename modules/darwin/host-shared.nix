468ba9031226feca0471773a412b06ad66be37a3
Pixdane <yuanjin233@gmail.com>
2025-10-05 22:19:19 +0800

    feat: enable Touch ID with sudo for darwin

--- modules/darwin/host-shared.nix
+++ modules/darwin/host-shared.nix
@@ -5,6 +5,7 @@
 }: {
   imports = with inputs.self.darwinModules; [
     activate-fish
+    sudo-local
   ];
 
   # List packages installed in system profile. To search by name, run:
---
+++ modules/darwin/sudo-local.nix
@@ -0,0 +1,11 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  security.pam.services.sudo_local = {
+    enable = true;
+    reattach = true;
+    touchIdAuth = true;
+  };
+}
