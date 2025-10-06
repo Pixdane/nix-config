b42a5ee5d11ada4ab74915c468270a8582534f5e
Pixdane <yuanjin233@gmail.com>
2025-10-06 11:07:10 +0800

    feat: update direnc config

--- modules/home/direnv.nix
+++ modules/home/direnv.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: {
   programs.direnv = {
     enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    # Already enabled by other config
+    # enableFishIntegration = true;
   };
 }
