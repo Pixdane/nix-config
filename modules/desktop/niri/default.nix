a8d2eed53816a29212318fbc5dff1a6f2358ff25
Pixdane <yuanjin233@gmail.com>
2025-10-07 19:17:33 +0800

    first attempt to use niri

---
+++ modules/desktop/niri/default.nix
@@ -0,0 +1,47 @@
+{
+  pkgs,
+  inputs,
+  niri-flake,
+  ...
+}: {
+  imports = [
+    # Login manager
+    ./greetd.nix
+
+    # File manager
+    ./dolphin.nix
+  ];
+
+  nixpkgs.overlays = [niri.overlays.niri];
+
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri-unstable;
+  };
+
+  xdg.portal.config.niri."org.freedesktop.impl.portal.FileChooser" = "gtk";
+
+  services.gnome.gnome-keyring.enable = true; # secret service
+
+  environment.systemPackages = with pkgs; [
+    xwayland-satellite
+    alacritty
+    fuzzel
+    # uwsm
+  ];
+
+  # NixOS 桌面配置 - Sway/Hyprland/Niri - 知乎
+  # https://zhuanlan.zhihu.com/p/1914081913351681037
+
+  # polkit agent
+  systemd.user.services.niri-flake-polkit.enable = false;
+  security.soteria.enable = true;
+
+  # 磁盘挂载
+  # services.gvfs.enable = true;
+
+  # 压缩解压
+  # programs.file-roller.enable = true;
+
+  security.pam.services.swaylock = {};
+}
---
+++ modules/desktop/niri/greetd.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --remember --remember-session --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
+      };
+    };
+  };
+}
