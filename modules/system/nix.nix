ef625d4ecb035e80c43c944fcaf4235790bf735d
Pixdane <yuanjin233@gmail.com>
2026-07-30 18:07:30 +0800

    feat(nix): add helix cachix binary cache

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -16,5 +16,9 @@
       "flakes"
     ];
     trusted-users = lib.mkForce (lib.unique ([ "root" ] ++ config.pixdane.system.nix.trustedUsers));
+
+    # helix flake 的二进制缓存
+    substituters = [ "https://helix.cachix.org" ];
+    trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
   };
 }
